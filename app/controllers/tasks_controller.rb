b52de04c87700d153786b1e5444bbe03e3b6d1e3
kenta.watanabe <boc.watyannn@gmail.com>
2020-06-16 06:19:34 +0000

    before_action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy,:edit,:show,:update]
   def index
     if logged_in?
       @task = current_user.tasks.build  # form_with 用
